94aa04a0f5a3e4ccc2e1d1d3a498ce875f015fe8
qualterz <qualterz@tutamail.com>
2022-12-25 16:36:37 +0200

    Add generated bindings

---
+++ src/lib_secp256k1/ecdh.cr
@@ -0,0 +1,7 @@
+@[Link(ldflags: "`command -v pkg-config > /dev/null && pkg-config --libs libsecp256k1 2> /dev/null|| printf %s '-lsecp256k1'`")]
+lib LibSecp256k1
+  $secp256k1_ecdh_hash_function_default : Secp256k1EcdhHashFunction
+  $secp256k1_ecdh_hash_function_sha256 : Secp256k1EcdhHashFunction
+  alias Secp256k1EcdhHashFunction = (UInt8*, UInt8*, UInt8*, Void* -> LibC::Int)
+  fun secp256k1_ecdh(ctx : Secp256k1Context, output : UInt8*, pubkey : Secp256k1Pubkey*, seckey : UInt8*, hashfp : Secp256k1EcdhHashFunction, data : Void*) : LibC::Int
+end
