4728bae8c9874280288e382a12bc5b75e8c2358b
qualterz <qualterz@tutamail.com>
2023-01-09 00:41:34 +0200

    Implement minimal schnorr example

--- examples/native.cr
+++ examples/native.cr
@@ -14,7 +14,13 @@ message = OpenSSL::Digest.new("SHA256").update("Hello, crypto!").final
 
 puts "SHA256 Hash Message Hex: #{message.hexstring}"
 
-signature, serialized_signature = create_ecdsa_signature context, message, secret_key
-signature_verification_status = verify_ecdsa_signature context, signature, message, public_key
+ecdsa_signature, ecdsa_serialized_signature = create_ecdsa_signature context, message, secret_key
+signature_verification_status = verify_ecdsa_signature context, ecdsa_signature, message, public_key
+
+keypair = create_keypair context
+public_key_xonly = create_xonly_public_key context, keypair
+
+schnorr_signature = create_schnorr_signature context, message, keypair
+signature_verification_status = verify_schnorr_signature context, schnorr_signature, message, public_key_xonly
 
 destroy_context context
--- examples/native/keypair.cr
+++ examples/native/keypair.cr
@@ -1,7 +1,7 @@
 require "../shared"
 
 def create_keypair(context)
-  keypair = LibSecp256k1.Secp256k1Keypair.new
+  keypair = LibSecp256k1::Secp256k1Keypair.new
 
   loop {
     status = LibSecp256k1.secp256k1_keypair_create(
@@ -13,5 +13,24 @@ def create_keypair(context)
     break if status == 1
   }
 
+  puts "Keypair Raw: #{keypair.data.to_slice.hexstring}"
+
   return keypair
+end
+
+def create_xonly_public_key(context, keypair)
+  public_key = LibSecp256k1::Secp256k1XonlyPubkey.new
+
+  if LibSecp256k1.secp256k1_keypair_xonly_pub(
+       context,
+       pointerof(public_key),
+       nil,
+       pointerof(keypair)
+     ) == 0
+    abort "Failed to create XOnly public key"
+  end
+
+  puts "XOnly Public Key Raw: #{public_key.data.to_slice.hexstring}"
+
+  return public_key
 end
