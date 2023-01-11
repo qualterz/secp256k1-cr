00020739e708f2542f791d3264b36517f273821a
qualterz <qualterz@tutamail.com>
2023-01-11 16:59:18 +0200

    Create context type enum

--- src/secp256k1/context.cr
+++ src/secp256k1/context.cr
@@ -1,11 +1,24 @@
 require "../lib_secp256k1"
 
 module Secp256k1
+  enum ContextType
+    None
+
+    def into_flag
+      case self
+      when ContextType::None
+        LibSecp256k1::SECP256K1_CONTEXT_NONE
+      else
+        LibSecp256k1::SECP256K1_CONTEXT_NONE
+      end
+    end
+  end
+
   class Context
     @inner_context : LibSecp256k1::Secp256k1Context
 
     def initialize
-      @inner_context = LibSecp256k1.secp256k1_context_create(LibSecp256k1::SECP256K1_CONTEXT_NONE)
+      @inner_context = LibSecp256k1.secp256k1_context_create(ContextType::None.into_flag)
 
       randomness = Random.new.random_bytes(32)
 
