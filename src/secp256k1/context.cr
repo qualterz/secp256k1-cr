e0faefbd86569fe3ff310421106179f5b02595d8
qualterz <qualterz@tutamail.com>
2023-01-12 13:50:52 +0200

    Use more secure randomization method

--- src/secp256k1/context.cr
+++ src/secp256k1/context.cr
@@ -20,15 +20,19 @@ module Secp256k1
     def initialize
       @inner_context = LibSecp256k1.secp256k1_context_create(ContextType::None.into_flag)
 
-      randomness = Random.new.random_bytes(32)
-
-      unless LibSecp256k1.secp256k1_context_randomize(@inner_context, randomness) == 1
+      unless LibSecp256k1.secp256k1_context_randomize(@inner_context, randomness(32)) == 1
         raise "Failed to randomize context"
       end
     end
 
     def finalize
       LibSecp256k1.secp256k1_context_destroy @inner_context
+    end
+
+    private def randomness(size)
+      randomness = Bytes.new(size)
+      Random::Secure.random_bytes(randomness)
+      return randomness
     end
   end
 end
