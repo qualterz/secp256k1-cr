461b11cb66144fbfa2054c885034339a533b4fa7
qualterz <qualterz@tutamail.com>
2023-01-15 14:11:18 +0200

    Implement keypair creation method from bytes

--- src/secp256k1/keypair.cr
+++ src/secp256k1/keypair.cr
@@ -12,6 +12,11 @@ module Secp256k1
   end
 
   class Keypair
+    class Error < Exception
+    end
+  end
+
+  class Keypair
     @wrapped_context : LibSecp256k1::Secp256k1Context
     @wrapped_keypair : LibSecp256k1::Secp256k1Keypair
 
@@ -34,20 +39,30 @@ module Secp256k1
   end
 
   class Context
+    def keypair_from(bytes : Bytes)
+      wrapped_keypair_instance = LibSecp256k1::Secp256k1Keypair.new
+
+      result = LibSecp256k1.secp256k1_keypair_create(
+        @wrapped_context,
+        pointerof(wrapped_keypair_instance),
+        bytes
+      )
+
+      unless result == Keypair::CreationResult::Valid.value
+        raise Keypair::Error.new "Secret key is invalid"
+      end
+
+      Keypair.new(@wrapped_context, wrapped_keypair_instance)
+    end
+
     def generate_keypair
-      wrapped_keypair = LibSecp256k1::Secp256k1Keypair.new
+      secret_key = Util.random_bytes(Keypair::SECRET_KEY_SIZE)
 
-      loop {
-        result = LibSecp256k1.secp256k1_keypair_create(
-          @wrapped_context,
-          pointerof(wrapped_keypair),
-          Util.random_bytes(Keypair::SECRET_KEY_SIZE)
-        )
-
-        break if result == Keypair::CreationResult::Valid.value
-      }
-
-      Keypair.new(@wrapped_context, wrapped_keypair)
+      begin
+        return keypair_from secret_key
+      rescue
+        raise Keypair::Error.new "Failed to generate keypair"
+      end
     end
   end
 end
