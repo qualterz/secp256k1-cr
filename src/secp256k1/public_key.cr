94c74c3f9b898734e0a1239e142f2c1451793dbc
qualterz <qualterz@tutamail.com>
2023-01-17 10:36:20 +0200

    Implement public key generation from bytes

--- src/secp256k1/public_key.cr
+++ src/secp256k1/public_key.cr
@@ -3,6 +3,20 @@ require "./util"
 
 module Secp256k1
   class PublicKey
+    enum CreationResult
+      Invalid = 0
+      Valid   = 1
+    end
+
+    SECRET_KEY_SIZE = 32
+  end
+
+  class PublicKey
+    class Error < Exception
+    end
+  end
+
+  class PublicKey
     @wrapped_context : LibSecp256k1::Secp256k1Context
     @wrapped_public_key : LibSecp256k1::Secp256k1Pubkey
 
@@ -11,6 +25,24 @@ module Secp256k1
 
     def bytes : Bytes
       @wrapped_public_key.data.to_slice
+    end
+  end
+
+  class Context
+    def public_key_from(bytes : Bytes)
+      wrapped_public_key_instance = LibSecp256k1::Secp256k1Pubkey.new
+
+      result = LibSecp256k1.secp256k1_ec_pubkey_create(
+        @wrapped_context,
+        pointerof(wrapped_public_key_instance),
+        bytes
+      )
+
+      unless result == PublicKey::CreationResult::Valid.value
+        raise PublicKey::Error.new "Secret key is invalid"
+      end
+
+      PublicKey.new(@wrapped_context, wrapped_public_key_instance)
     end
   end
 end
